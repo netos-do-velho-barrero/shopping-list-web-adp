097ad0c705154f98360b576f96c67dd1e966055e
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-06-06 12:37:45 -0300

    chore: uncomment and configure default seed data registration in Program.cs

--- ShoppingList.WebApp/Compartilhado/Apresentacao/Extensions/ModelStateExtensions.cs
+++ ShoppingList.WebApp/Compartilhado/Apresentacao/Extensions/ModelStateExtensions.cs
@@ -5,15 +5,15 @@ namespace ListaDeComprasWeb.WebApp.Compartilhado.Apresentacao.Extensions;
 
 public static class ModelStateExtensions
 {
-    // public static void AddModelError(this ModelStateDictionary modelState, ResultBase result)
-    // {
-    //     foreach (IError erro in result.Errors)
-    //     {
-    //         string campo = erro.Metadata["Campo"] is string
-    //             ? erro.Metadata["Campo"].ToString()!
-    //             : string.Empty;
+    public static void AddModelError(this ModelStateDictionary modelState, ResultBase result)
+    {
+        foreach (IError erro in result.Errors)
+        {
+            string campo = erro.Metadata["Campo"] is string
+                ? erro.Metadata["Campo"].ToString()!
+                : string.Empty;
 
-    //         modelState.AddModelError(campo, erro.Message);
-    //     }
-    // }
+            modelState.AddModelError(campo, erro.Message);
+        }
+    }
 }
--- ShoppingList.WebApp/Program.cs
+++ ShoppingList.WebApp/Program.cs
@@ -1,6 +1,35 @@
+using ListaDeComprasWeb.WebApp.Compartilhado.Aplicacao;
+using ListaDeComprasWeb.WebApp.Compartilhado.Apresentacao;
+using ListaDeComprasWeb.WebApp.Compartilhado.Infra;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// feito para facilitar a implementação dos demais módulos com uma base de injeções e compartilhamento já criada.
+
+// registra os repositórios da camada de Infra.
+// conforme os módulos forem criados, descomentar os registros dentro de:
+// compartilhado/Infra/InjecaoDependencia.cs
+builder.Services.AddInfraRepositories();
+
+// registra os serviços da camada de Aplicação.
+// conforme os módulos forem criados, descomente os registros dentro de:
+// compartilhado/Aplicacao/InjecaoDependencia.cs
+builder.Services.AddApplicationServices();
+
+// registra Controllers, Razor Views e AutoMapper.
+// conforme os módulos forem criados, descomente os Profiles dentro de:
+// compartilhado/Apresentacao/InjecaoDependencia.cs
+builder.Services.AddPresentation();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+
+app.UseRouting();
+
+app.MapDefaultControllerRoute();
 
 app.Run();
